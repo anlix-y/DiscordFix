466e66532cfbb554e6ffad72c4bebaf39a3857c0
Anlixy <alex19mu9kg26@gmail.com>
2024-11-04 17:10:12 +0300

    RELESES

--- ALLremove.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-:: 65001 - UTF-8
-
-:: Завершение процесса winws.exe с помощью PowerShell
-powershell -Command "Get-Process -Name winws -ErrorAction SilentlyContinue | ForEach-Object { $_.Kill() }"
-
-echo Процесс winws.exe завершен.
-pause
--- web.ps1
+++ web.ps1
@@ -1,6 +1,14 @@
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients for current session
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "You do not have sufficient privileges. Please run the script as an administrator."
+    exit
+}
 
 $destinationFolder = "$env:APPDATA\DiscordFix"
 if (-not (Test-Path -Path $destinationFolder)) {
@@ -9,7 +17,7 @@ if (-not (Test-Path -Path $destinationFolder)) {
 
 function Check-DiscordStatus {
     try {
-        $response = Invoke-RestMethod -Uri 'https://discord.com/api/v9/gateway' -TimeoutSec 10
+        $response = Invoke-RestMethod -Uri 'https://discord.com/api/v9/gateway' -TimeoutSec 5
         if ($response) {
             Write-Host "Discord API is accessible."
             return $true
@@ -24,7 +32,6 @@ function Check-DiscordStatus {
 }
 
 function FindAndRunBatFiles {
-    # Указываем путь к директории DiscordFix
     $directoryPath = "$env:APPDATA\DiscordFix"
 
     # Проверяем, существует ли директория
@@ -54,36 +61,109 @@ function FindAndRunBatFiles {
     }
 
     foreach ($file in $batFiles) {
-
-        # Читаем содержимое файла
         $content = Get-Content -Path $file.FullName
 
         # Проверяем, содержит ли файл слова "general" или "discord"
         if (($content -match 'general' -or $content -match 'discord') -and -not ($content -match 'service')) {
-            "Running $($file.FullName)"
+            Write-Host "Running $($file.FullName)"
 
-            if (Check-DiscordStatus)
-            {
-                Write-Host "Discord is working correctly."
+            $batFilePath = $file.FullName
+            $workingDirectory = Split-Path -Path $batFilePath
+            $taskName = "TempFIX_" + [guid]::NewGuid().ToString()
+            schtasks /create /tn $taskName /tr "`"$batFilePath`"" /sc once /st 00:00 /rl highest /f /it /ru System
+            schtasks /run /tn $taskName
+            Start-Sleep -Seconds 2
+            schtasks /delete /tn $taskName /f
+
+            # Start-Process -FilePath $file.FullName -WindowStyle Hidden -NoNewWindow
+            Start-Sleep -Seconds 3
+            if (Check-DiscordStatus) {
+                Clear-Host
+                
+                Write-Host "1 - добавить в автозагрузку"
+                Write-Host "2 - не добовлять в автозагрузки"
+
+                $choice = Read-Host "Введите номер действия"
+
+                switch ($choice) {
+                    1 {
+                        Clear-Host
+                        $batFilePath = $file.FullName
+                        $workingDirectory = Split-Path -Path $batFilePath
+                        $taskName = "DiscordFix"
+                        schtasks /delete /tn $taskName /f 2>$null
+                        schtasks /create /tn $taskName /tr "`"$batFilePath`"" /sc onlogon /rl highest /f /it /ru System
+                        Write-Host "Задача '$taskName' успешно добавлена в автозапуск."
+                        Read-Host "Нажмите enter что бы продолжить..."
+                        Clear-Host
+                        Show-Menu-start
+                    }
+                    2 {
+                        Clear-Host
+                        Read-Host "Нажмите enter что бы продолжить..."
+                        Clear-Host
+                        Show-Menu-start
+                    }
+                    default {
+                        Read-Host "Неверный выбор."
+                    }
+                }
             }
+            Stop-WinwsProcess
         }
     }
     Write-Host "Discord is not working correctly."
 }
 
-function Show-Menu {
-    param (
-        [switch]$ClearScreen = $true
-    )
+function Show-Menu-start {
+    Clear-Host
+    Write-Host "╔═══╗                   ╔╗ ╔═══╗      "
+    Write-Host "╚╗╔╗║                   ║║ ║╔══╝      "
+    Write-Host " ║║║║╔╗╔══╗╔══╗╔══╗╔═╗╔═╝║ ║╚══╗╔╗╔╗╔╗"
+    Write-Host " ║║║║╠╣║══╣║╔═╝║╔╗║║╔╝║╔╗║ ║╔══╝╠╣╚╬╬╝"
+    Write-Host "╔╝╚╝║║║╠══║║╚═╗║╚╝║║║ ║╚╝║╔╝╚╗  ║║╔╬╬╗"
+    Write-Host "╚═══╝╚╝╚══╝╚══╝╚══╝╚╝ ╚══╝╚══╝  ╚╝╚╝╚╝"
+    Write-Host 
+    Write-Host "Выберите действие:"
+    Write-Host "1 - Установить\обновить"
+    Write-Host "2 - Открыть меню работы с файлами"
+    Write-Host "3 - Удалить"
+    Write-Host "0 - Выход"
 
-    if ($ClearScreen) {
-        Clear-Host
+    $choice = Read-Host "Введите номер действия"
+
+    switch ($choice) {
+        1 {
+            Clear-Host
+            Stop-WinwsProcess
+            Start-install
+            Show-Menu-install
+        }
+        2 {
+            Show-Menu-install
+        }
+        3 {
+            Clear-Host
+            Remove-DiscordFixDirectory
+        }
+        0 {
+            exit
+        }
+        default {
+            Read-Host "Неверный выбор."
+            Clear-Host
+            Show-Menu-start -ClearScreen $false
+        }
     }
+}
 
+function Show-Menu-install {
+    Clear-Host
+    
     Write-Host "Выберите действие:"
     Write-Host "1 - Авто настройка"
     Write-Host "2 - Ручной выбор"
-    Write-Host "3 - Удалить из авто запуска"
+    Write-Host "3 - Удалить из автозапуска"
     Write-Host "4 - Снаять задачу"
     Write-Host "0 - Выход"
 
@@ -105,90 +185,141 @@ function Show-Menu {
         4 {
             Clear-Host
             Stop-WinwsProcess
+            Read-Host "Нажмите Enter что бы продолжить..."
         }
         0 {
             exit
         }
         default {
             Write-Host "Неверный выбор. Пожалуйста, попробуйте снова."
-            Show-Menu -ClearScreen $false
+            Show-Menu-install -ClearScreen $false
         }
+    }
+}
+
+
+#! Функции работы с удалением
+
+function Remove-DiscordFixDirectory {
+    # Указываем путь к директории DiscordFix
+    $directoryPath = "$env:APPDATA\DiscordFix"
+
+    # Проверяем, существует ли директория
+    if (Test-Path -Path $directoryPath) {
+        try {
+            # Удаляем директорию и все её содержимое
+            Remove-Item -Path $directoryPath -Recurse -Force
+            Write-Host "Успешно!!!"
+        } catch {
+            Write-Host "Ошибка удаления!"
+        }
+    } else {
+        Write-Host "Папки не существует..."
     }
 }
 
 function Manual-Selection {
     Write-Host "В разработке..."
-    # Здесь ваш код для ручного выбора
+    Show-Menu-start
 }
 
 function Remove-StartupShortcut {
-    param (
-        [string]$shortcutName = "Start Winws DsFix1.lnk"
-    )
+    $taskName = "DiscordFix"
+    $taskExists = schtasks /query /tn $taskName 2>$null
 
-    # Путь к папке автозапуска
-    $startupFolder = [System.IO.Path]::Combine($env:APPDATA, "Microsoft\Windows\Start Menu\Programs\Startup")
-
-    # Проверяем существование ярлыка
-    $shortcutPath = [System.IO.Path]::Combine($startupFolder, $shortcutName)
-    if (Test-Path -Path $shortcutPath) {
-        Remove-Item -Path $shortcutPath
-        Write-Host "Ярлык для автозапуска удален."
+    if ($taskExists) {
+        schtasks /delete /tn $taskName /f
+        Write-Host "Задача '$taskName' успешно удалена из автозапуска."
     } else {
-        Write-Host "Ярлык для автозапуска не найден."
+        Write-Host "Задача '$taskName' не найдена. Нечего удалять."
     }
-    
-    Stop-WinwsProcess
-    # Пауза перед закрытием окна
-    Read-Host "Нажмите Enter для выхода..."
 }
 
 
 function Stop-WinwsProcess {
-    # Завершение процесса winws.exe
     Get-Process -Name winws -ErrorAction SilentlyContinue | ForEach-Object { $_.Kill() }
-
     Write-Host "Процесс winws.exe завершен."
-
-    # Пауза перед закрытием окна
-    Read-Host "Нажмите Enter для выхода..."
 }
 
-# Function to download files and directories recursively
-function Download-Repository {
+
+#! Функции работы с загрузкой
+
+function Download-File {
     param (
-        [string]$repoURL,
-        [string]$destinationFolder
+        [string]$Url,
+        [string]$File
     )
+    Write-Host "Downloading $Url to $File"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri $Url -OutFile $File
+}
 
-    $files = Invoke-RestMethod -Uri $repoURL
-
-    foreach ($file in $files) {
-        if ($file.type -eq 'file') {
-            $downloadURL = $file.download_url
-            $fileName = $file.name
-            $filePath = Join-Path -Path $destinationFolder -ChildPath $fileName
-            try {
-                $response = Invoke-WebRequest -Uri $downloadURL -UseBasicParsing
-                Set-Content -Path $filePath -Value $response.Content
-            }
-            catch {
-                Write-Warning "Failed to retrieve $downloadURL, skipping..."
-            }
-        } elseif ($file.type -eq 'dir') {
-            $dirName = $file.name
-            $dirPath = Join-Path -Path $destinationFolder -ChildPath $dirName
-            if (-not (Test-Path -Path $dirPath)) {
-                New-Item -ItemType Directory -Path $dirPath
-            }
-            Download-Repository -repoURL $file.url -destinationFolder $dirPath
-        }
+function Remove-Directory {
+    param (
+        [string]$Path
+    )
+    if ([System.IO.Directory]::Exists($Path)) {
+        Write-Host "Deleting $Path"
+        [System.IO.Directory]::Delete($Path, $true)
     }
 }
 
-# Download all files and directories from https://github.com/Flowseal/zapret-discord-youtube
-$repoURL = 'https://api.github.com/repos/Flowseal/zapret-discord-youtube/contents/'
-Download-Repository -repoURL $repoURL -destinationFolder $destinationFolder
+function Unzip-File {
+    param(
+        [string]$File,
+        [string]$Destination = (Get-Location).Path
+    )
 
-Show-Menu
+    $FilePath = Resolve-Path $File
+    $DestinationPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Destination)
 
+    If ($PSVersionTable.PSVersion.Major -ge 3) {
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$FilePath", "$DestinationPath")
+    }
+    else {
+        throw "Unzip-File is not supported on PowerShell versions less than 3.0"
+    }
+}
+
+function Move-DirectoryContent {
+    param (
+        [string]$Source,
+        [string]$Destination
+    )
+
+    $SourcePath = Resolve-Path $Source
+    $DestinationPath = Resolve-Path $Destination
+
+    Write-Output "Moving content of $SourcePath to $DestinationPath"
+    Get-ChildItem -Path $SourcePath -Recurse | Move-Item -Destination $DestinationPath
+}
+
+function Start-install {
+    $UserName = "Flowseal"
+    $RepoName = "zapret-discord-youtube"
+    $Branch = "main"
+
+    $DotfileTmpDir = $env:TEMP
+    $SourceFile = Join-Path $DotfileTmpDir "zapret-discord-youtube.zip"
+    $DestPath = "$env:APPDATA\DiscordFix"
+    # Скачивание репозитория
+    Download-File "https://github.com/$UserName/$RepoName/archive/refs/heads/$Branch.zip" $SourceFile
+
+    # Удаление существующей папки назначения
+    Remove-Directory $DestPath
+
+    # Распаковка архива
+    Unzip-File $SourceFile $DestPath
+
+    # Путь к распакованной папке
+    $ZipOutputDir = Join-Path $DestPath "$RepoName-$Branch"
+
+    # Перемещение содержимого распакованной папки в папку назначения
+    Move-DirectoryContent $ZipOutputDir $DestPath
+
+    # Удаление временной папки
+    Remove-Directory $ZipOutputDir
+}
+
+
+Show-Menu-start
